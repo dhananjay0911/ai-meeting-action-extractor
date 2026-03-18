183583691caef899b9bbbffd16391cb3f0bc2251
dhananjay0911 <dhananjaybamhande@gmail.com>
2026-03-18 15:59:30 +0530

    fix missing files

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from "react";
 import { getTasks } from "../services/api";
 import TaskCard from "./TaskCard";
 import "./TaskList.css";
-import "./TaskList.css";
 
 function TaskList({transcriptId}){
 
